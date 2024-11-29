17b7a77d7de6cc52a024c2fa50f3d8eeb4003c8c
admintran <duong170226@gmail.com>
2024-11-29 16:40:41 +0700

    Update Ss9_bai 4.cpp

--- Ss9_bai 4.cpp
+++ Ss9_bai 4.cpp
@@ -1,8 +1,9 @@
+
 #include<stdio.h>
 int main(){
 	int a[100];
 	int n;
-	printf("1.Nhap so \n2.In mang\n3.Them phan tu\n4.Sua phan tu\n5.Xoa phan tu\n6.Thoat\n);
+	printf("1.Nhap so \n2.In mang\n3.Them phan tu\n4.Sua phan tu\n5.Xoa phan tu\n6.Thoat\n");
 	do{
 	int choice;
 	printf("Moi ban nhap lua chon (1-6):");
@@ -23,32 +24,32 @@ int main(){
 		printf("\n");	
 	}
 	else if(choice==3){
-		printf("Nhap vao vi tri muon them :");
+		printf("Nhap vao vi tri muon them (0-%d):",n+1);
 		int pos;										
 		scanf("%d",&pos);
-		if(pos<0||pos>n+1){
-		return 0;
+		if(pos==0){
+			pos=1;
+		}
+		if(pos<1||pos>n+1){
+			printf("Vi tri khong hop le!\n");	
+			continue;
 		}	
 		printf("Nhap gia tri cho phan tu muon them :");
 		int value;
 		scanf("%d",&value);
-		for(int i=0;i<n;i++){
-			if(pos>=n){
-			a[pos-1]=value;
-			}
-			if(pos<n&&i>=pos-1){	
-			a[n+pos-i-1]=a[n+pos-i-2];
-			}
+		for(int i=n;i>=pos;i--){
+			a[i]=a[i-1];
 		}
 		a[pos-1]=value;
 		n+=1;
 	}
 	else if(choice==4){
-		printf("Nhap vao vi tri muon sua :");
+		printf("Nhap vao vi tri muon sua (1-%d) :",n);
 		int pos_fix;										
 		scanf("%d",&pos_fix);
-		if(pos_fix<0||pos_fix>n){
-		return 0;
+		if(pos_fix<1||pos_fix>n){
+			printf("Vi tri khong hop le!\n");
+			continue;
 		}	
 		printf("Nhap gia tri muon sua :");
 		int value_fix;
@@ -60,11 +61,12 @@ int main(){
 		}
 	}
 	else if(choice==5){
-		printf("Nhap vao vi tri muon xoa :");
+		printf("Nhap vao vi tri muon xoa (1-%d):",n);
 		int pos_clear;										
 		scanf("%d",&pos_clear);
-		if(pos_clear<0||pos_clear>n){
-		return 0;
+		if(pos_clear<1||pos_clear>n){
+			printf("Vi tri khong hop le !\n");
+			continue;
 		}	
 		for(int i=0;i<n;i++){
 			if(i>=pos_clear-1){
